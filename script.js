191778719b3d8ebb3efbe2e782b365d808d587a8
RachitRawat09 <rawatritik5670@gmail.com>
2025-03-03 20:27:57 +0530

    add changes 2

---
+++ script.js
@@ -0,0 +1,19 @@
+var tl=gsap.timeline()
+tl.from('.logo,.nav-items li ',{
+    y:-30,
+    duration:0.5,
+    opacity:0,
+    stagger:0.3,
+    ease: "power2.inOut"
+})
+tl.from('.main-content ',{
+    y:-30,
+    duration:0.5,
+    opacity:0,
+    stagger:0.3,
+    ease: "power2.inOut"
+})
+var t2=gsap.scrollTrigger()
+t2.from('',{
+    
+})
